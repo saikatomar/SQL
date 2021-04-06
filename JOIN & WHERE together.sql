3824690fad48700eb981fc9f12d7d6afec8f95f8
Saikat Omar Khan <77507686+saikatomar@users.noreply.github.com>
2021-04-06 21:15:43 -0230

    Add files via upload

---
+++ JOIN & WHERE together.sql
@@ -0,0 +1,21 @@
+# Using JOIN and WHERE together
+SELECT e.emp_no, e.first_name, e.last_name, s.salary
+FROM employees e 
+	JOIN 
+		salaries s ON e.emp_no = s.emp_no 
+WHERE 
+	s.salary > 145000;
+    
+# Practice: Select the first and last name, the hire date, and the job title of 
+# all employees whose first name is “Margareta” and have the last name “Markovitch”.
+
+SELECT 
+    e.first_name, e.last_name, e.hire_date, t.title
+FROM
+    employees e
+        JOIN
+    titles t ON e.emp_no = t.emp_no
+WHERE
+    e.first_name = 'Margareta'
+        AND e.last_name = 'Markovitch'
+ORDER BY e.emp_no;
